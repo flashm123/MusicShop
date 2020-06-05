6c40519bda1840ad7f6dce2e5c7ce8862d2fbd0d
flashm123 <>
2020-06-05 23:55:53 +0300

    Add parameter constants; add category view and model

---
+++ Models/CategoryModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicShop.Models
+{
+    public class CategoryModel
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+}
