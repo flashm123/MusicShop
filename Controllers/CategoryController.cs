65be984f2683884f40d63d11ed6faad74daaea62
flashm123 <>
2020-06-06 00:00:22 +0300

    Add category controller

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using MusicShop.Constants;
+using MusicShop.Models;
+
+namespace MusicShop.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index(int id)
+        {
+            var model = new CategoryModel() 
+            { 
+                Name = Enum.GetName(typeof(CategoryParameters.CategoryNumber), id) 
+            };
+
+            return View(model);
+        }
+    }
+}
